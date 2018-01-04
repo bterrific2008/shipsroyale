53cd0bf49f0071b41b9bc6b8dcb319501a05ee7e
Arie Chen <32532088+arie-chen@users.noreply.github.com>
2018-01-03 17:20:42 -0800

    Created Grid
    Created basic layout.

--- gui/BoardPanel.java
+++ gui/BoardPanel.java
@@ -1,7 +1,20 @@
 package gui;
 
+import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class BoardPanel extends JPanel{
 	public BoardPanel() {};
+	
+	@Override
+	public void paint(Graphics g) {
+		int width = this.getWidth();		//width of board game panel
+		int height = this.getHeight();		//height of board game panel
+		
+		makeGrid(width, height, g);
+	}
+	
+	private void makeGrid(int width, int height, Graphics g) {
+		g.drawRect(0, 0, width, height);;
+	}
 }
