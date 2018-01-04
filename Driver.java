282394c6ac99fe32e348ab3318823e7a5f78e852
ningmenhao <purelymadgenius@gmail.com>
2018-01-03 17:38:45 -0800

    Added Driver.java updates
    Updated GUI branch with the changes made in master branch to Driver.java

--- Driver.java
+++ Driver.java
@@ -1,5 +1,7 @@
+import java.awt.Dimension;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 import battleship.*;
 import gui.*;
@@ -9,6 +11,17 @@ public class Driver {
 
 	public static void main(String args[]){
 		JFrame frame = new JFrame();
+		JPanel panel = new BoardPanel();
+		
+		Dimension preferredDimension = new Dimension(1000, 1000);
+		panel.setPreferredSize(preferredDimension);
+		
+		frame.add(panel);
+		frame.pack();
+		frame.setVisible(true);
 	}
 	
 }
+
+	
+
