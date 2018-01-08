0b4ea166b7194c92cf58257e16ce1d4d2cb27a34
ningmenhao <purelymadgenius@gmail.com>
2018-01-08 15:40:56 -0800

    I have no idea what I'm doing

--- Driver.java
+++ Driver.java
@@ -15,23 +15,9 @@ public class Driver {
 	public static void main(String args[]){
 		JFrame frame = new JFrame();
 		JPanel panel = new BoardPanel();
-		
-<<<<<<< HEAD
-<<<<<<< HEAD
-		
-		
-		
 		Dimension preferredDimension = new Dimension(1000, 1000);
-=======
-		Dimension preferredDimension = new Dimension(700, 700);
->>>>>>> GUI
 		panel.setPreferredSize(preferredDimension);
 		frame.setResizable(true);
-=======
-		Dimension preferredDimension = new Dimension(1000, 1000);
-		panel.setPreferredSize(preferredDimension);
-		
->>>>>>> refs/remotes/origin/master
 		frame.add(panel);
 		frame.pack();
 		frame.setVisible(true);
--- gui/BoardPanel.java
+++ gui/BoardPanel.java
@@ -3,7 +3,6 @@ package gui;
 import java.awt.BasicStroke;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-<<<<<<< HEAD
 
 import javax.swing.JPanel;
 
@@ -11,19 +10,6 @@ import battleship.Ship;
 import battleship.ShipSection;
 import battleship.Ship.Directions;
 
-public class BoardPanel extends JPanel{
-	Ship ship;
-	
-	public BoardPanel() {
-		ship = new Ship(9, 0, Directions.HORIZONTAL, 4);
-		
-		ShipSection[] sections = ship.getSections();
-		
-		for(ShipSection s:sections){
-			System.out.println(s.getX()+" "+s.getY());
-		}
-	};
-=======
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -33,22 +19,20 @@ import javax.swing.JPanel;
 public class BoardPanel extends JPanel implements MouseListener{
 	public BoardPanel() {};
 	public int margin = 10;
-	
->>>>>>> GUI
-	
+
+
 	@Override
 	public void paint(Graphics g) {
 		int width = this.getWidth();		//width of board game panel
 		int height = this.getHeight();		//height of board game panel
 		makeGrid(width, height, g);
 	}
-	
-<<<<<<< HEAD
+
 	private void makeGrid(int width, int height, Graphics g) {
 		Graphics2D g2 = (Graphics2D) g;
 		int block = (width-(2*margin))/10;	//each individual brick
 		g2.setStroke(new BasicStroke(3));
-		
+
 		for(int w = margin; w <= width; w = w+block) {
 			g2.drawLine(w, margin, w, height-margin);
 			g2.drawLine(margin, w, width-margin, w);
@@ -59,33 +43,30 @@ public class BoardPanel extends JPanel implements MouseListener{
 	public void mouseClicked(MouseEvent e) {
 		int x = e.getX();
 		int y = e.getY();
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent arg0) {
-		
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
-=======
-	
-	
->>>>>>> refs/remotes/origin/master
+
 }
