751a488490a6af4b7c92f4bb867ff8e4088fef58
ningmenhao <purelymadgenius@gmail.com>
2018-01-08 15:24:22 -0800

    Revert "Refined JFrame"
    This reverts commit 88f3212358dd1a2e70997a73c6bc8b5f15cd47e7.

---
+++ Driver.java
@@ -0,0 +1,14 @@
+
+import javax.swing.JFrame;
+
+import battleship.*;
+import gui.*;
+import board.*;
+
+public class Driver {
+
+	public static void main(String args[]){
+		JFrame frame = new JFrame();
+	}
+	
+}
